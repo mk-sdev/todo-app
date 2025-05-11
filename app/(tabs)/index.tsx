8fa71408d91fcdef70505d21597061f5fa32c4b9
Mikołaj <kaleta222@gmail.com>
2025-05-11 19:50:13 +0200

    move some functions from index.tsx to functions.ts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,13 @@
-import { CyclicTask, Task } from '@/types'
+import {
+  checkCyclicTasks,
+  getOldTasks,
+  getOverdueTasks,
+  getTasksState,
+  getTasksToRemove,
+  removeTasks,
+  toggleTaskCompletionAsync
+} from '@/functions'
+import { Task } from '@/types'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useFocusEffect } from 'expo-router'
 import { useCallback, useState } from 'react'
@@ -11,99 +20,14 @@ export default function HomeScreen() {
   const [day, setDay] = useState<Date>(new Date())
   // AsyncStorage.clear()
 
-  async function setCyclicInstance(cyclicTasks: CyclicTask[]) {
-    console.log('🚀 ~ setCyclicInstance ~ cyclicTasks:', cyclicTasks)
-    try {
-      const storedTasks = await AsyncStorage.getItem('tasks')
-      if (storedTasks) {
-        const parsedTasks: Task[] = JSON.parse(storedTasks)
-
-        // Dodaj tylko te zadania z cyclicTasks, których tytuły ORAZ daty nie występują w parsedTasks
-        const filteredCyclicTasks = cyclicTasks.filter(
-          cyclicTask =>
-            !parsedTasks.some(
-              existingTask =>
-                existingTask.title === cyclicTask.title &&
-                existingTask.date === day.toLocaleDateString('en-CA')
-            )
-        )
-
-        const cyclicTasksInstances: Task[] = filteredCyclicTasks.map(
-          cyclicTask => ({
-            title: cyclicTask.title,
-            difficulty: cyclicTask.difficulty,
-            priority: cyclicTask.priority,
-            date: day.toLocaleDateString('en-CA'),
-            time: cyclicTask.time,
-            completed: false,
-            dateOfCompletion: null,
-            parent: cyclicTask.title,
-          })
-        )
-
-        console.log(
-          '🚀 ~ setCyclicInstance ~ newCyclicTasks:',
-          filteredCyclicTasks
-        )
-
-        const newTasks = [...parsedTasks, ...cyclicTasksInstances]
-        await AsyncStorage.setItem('tasks', JSON.stringify(newTasks))
-      } else {
-        // Jeśli nie ma zapisanych zadań, zapisujemy wszystkie z cyclicTasks
-        await AsyncStorage.setItem('tasks', JSON.stringify(cyclicTasks))
-      }
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   useFocusEffect(
     useCallback(() => {
       ;(async () => {
         //** sprawdza czy na dany dzień wypadają jakies zadania cykliczne */
         const cyclicTasks = await AsyncStorage.getItem('cyclicTasks')
 
-        const todayWeekDay = day
-          .toLocaleDateString('en-US', { weekday: 'short' })
-          .toLowerCase() // np. 'mon'
-        const todayDate = day.getDate() // dzień miesiąca
-
         if (cyclicTasks) {
-          const parsedCyclicTasks = JSON.parse(cyclicTasks)
-          const cyclicTasksForThatDay: CyclicTask[] = []
-
-          parsedCyclicTasks.forEach((task: CyclicTask) => {
-            const baseDate = new Date(task.date)
-            const diffDays = Math.floor(
-              (day.getTime() - baseDate.getTime()) / (1000 * 60 * 60 * 24)
-            )
-
-            switch (task.type) {
-              case 0:
-                //@ts-ignore // co X dni
-                if (diffDays % task.period === 0)
-                  //dodaj instancję do listy
-                  cyclicTasksForThatDay.push(task)
-
-                break
-
-              case 1:
-                // w określone dni tygodnia
-                if (task?.weekDays?.includes(todayWeekDay as Day))
-                  cyclicTasksForThatDay.push(task)
-
-                break
-
-              case 2:
-                // w określone dni miesiąca
-                if (task?.monthDays?.includes(todayDate))
-                  cyclicTasksForThatDay.push(task)
-
-                break
-            }
-          })
-          //** jeśli wypadają, to są tworzone ich instancje z odpowiednią datą, o ile nie zostały już wcześniej utworzone */
-          await setCyclicInstance(cyclicTasksForThatDay)
+          await checkCyclicTasks(day, cyclicTasks)
         }
 
         //* tutaj tworzony jest stan zadań do wyświetlenia dla usera
@@ -112,77 +36,30 @@ export default function HomeScreen() {
           const parsedTasks = JSON.parse(storedTasks)
 
           // stare zadania, czyli z datą wcześniejszą niż dziś
-          const oldTasks = parsedTasks.filter(
-            (task: Task) => task.date < new Date().toLocaleDateString('en-CA')
-          )
+          const oldTasks = getOldTasks(parsedTasks)
 
           // zaległe zadania do wyświetlenia
-          const overdueTasks = oldTasks.filter(
-            (task: Task) =>
-              (!task.dateOfCompletion &&
-                day.toLocaleDateString('en-CA') ===
-                  new Date().toLocaleDateString('en-CA')) ||
-              task.dateOfCompletion === day.toLocaleDateString('en-CA')
-          )
+          const overdueTasks = getOverdueTasks(oldTasks, day)
 
           //? co jak zaległe zadanie jest cykliczne?
           // zadania z daną datą (cykliczne i zwykłe) + zaległe do wyświetlenia
-          const tasksState = [
-            ...overdueTasks,
-            ...parsedTasks.filter(
-              (task: Task) => task.date === day.toLocaleDateString('en-CA')
-            ),
-          ]
+          const tasksState = getTasksState(overdueTasks, parsedTasks, day)
+
           // jeśli zaległe były takie same jak zaplanowane na dziś to nie duplikuj
           const uniqueTasksState = Array.from(
             new Map(tasksState.map(task => [task.title, task])).values()
           )
 
           setTasks(uniqueTasksState)
-          oldTasks.forEach((task: Task) => {
-            console.log(
-              'tytul, data wykonania',
-              task.title,
-              task.dateOfCompletion
-            )
-          })
 
           // zadania wykonane w przeszłości
-          const tasksToRemove = oldTasks.filter(
-            (task: Task) =>
-              (task.dateOfCompletion &&
-                task.dateOfCompletion <
-                  new Date().toLocaleDateString('en-CA')) ||
-              (uniqueTasksState.some(t => t.title === task.title) &&
-                task.parent !== null)
-          )
+          const tasksToRemove = getTasksToRemove(oldTasks, uniqueTasksState)
           //jeśli dobrze myślę to overdueTasks + tasksToRemove = oldTasks
           removeTasks(tasksToRemove)
         }
       })()
     }, [day])
   )
-
-  async function removeTasks(tasksToRemove: Task[]) {
-    console.log('🚀 ~ do usunięcia:')
-    tasksToRemove.forEach(task => {
-      console.log(task.title, task.date)
-    })
-    try {
-      const storedTasks = await AsyncStorage.getItem('tasks')
-      if (!storedTasks) return
-
-      const parsedTasks: Task[] = JSON.parse(storedTasks)
-
-      const updatedTasks = parsedTasks.filter(
-        task => !tasksToRemove.some(toRemove => toRemove.title === task.title)
-      )
-
-      await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks))
-    } catch (error) {
-      console.error('Error removing tasks:', error)
-    }
-  }
 
   function toggleTaskCompletion(title: string) {
     setTasks(prevTasks =>
@@ -199,30 +76,6 @@ export default function HomeScreen() {
       )
     )
     toggleTaskCompletionAsync(title)
-  }
-
-  async function toggleTaskCompletionAsync(title: string) {
-    try {
-      const stored = await AsyncStorage.getItem('tasks')
-      if (!stored) return
-
-      const tasks = JSON.parse(stored)
-      const updatedTasks = tasks.map((task: Task) =>
-        task.title === title
-          ? {
-              ...task,
-              completed: !task.completed,
-              dateOfCompletion: !task.completed
-                ? new Date().toLocaleDateString('en-CA')
-                : null,
-            }
-          : task
-      )
-
-      await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks))
-    } catch (e) {
-      console.error('Błąd podczas zapisywania zadania:', e)
-    }
   }
 
   return (
---
+++ functions.ts
@@ -0,0 +1,199 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { CyclicTask, Day, Task } from './types'
+
+export async function setCyclicInstance(
+  cyclicTasks: CyclicTask[],
+  day: Date
+): Promise<void> {
+  console.log('🚀 ~ setCyclicInstance ~ cyclicTasks:', cyclicTasks)
+  try {
+    const storedTasks = await AsyncStorage.getItem('tasks')
+    if (storedTasks) {
+      const parsedTasks: Task[] = JSON.parse(storedTasks)
+
+      // Dodaj tylko te zadania z cyclicTasks, których tytuły ORAZ daty nie występują w parsedTasks
+      const filteredCyclicTasks = getFilteredCyclicTasks(
+        cyclicTasks,
+        parsedTasks,
+        day
+      )
+
+      const cyclicTasksInstances: Task[] = getCyclicTasksInstances(
+        filteredCyclicTasks,
+        day
+      )
+
+      const newTasks = [...parsedTasks, ...cyclicTasksInstances]
+      await AsyncStorage.setItem('tasks', JSON.stringify(newTasks))
+    } else {
+      // Jeśli nie ma zapisanych zadań, zapisujemy wszystkie z cyclicTasks
+      await AsyncStorage.setItem('tasks', JSON.stringify(cyclicTasks))
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+function getFilteredCyclicTasks(
+  cyclicTasks: CyclicTask[],
+  parsedTasks: Task[],
+  day: Date
+): CyclicTask[] {
+  return cyclicTasks.filter(
+    (cyclicTask: CyclicTask) =>
+      !parsedTasks.some(
+        (existingTask: Task) =>
+          existingTask.title === cyclicTask.title &&
+          existingTask.date === day.toLocaleDateString('en-CA')
+      )
+  )
+}
+
+function getCyclicTasksInstances(
+  filteredCyclicTasks: CyclicTask[],
+  day: Date
+): Task[] {
+  return filteredCyclicTasks.map(cyclicTask => ({
+    title: cyclicTask.title,
+    difficulty: cyclicTask.difficulty,
+    priority: cyclicTask.priority,
+    date: day.toLocaleDateString('en-CA'),
+    time: cyclicTask.time,
+    completed: false,
+    dateOfCompletion: null,
+    parent: cyclicTask.title,
+  }))
+}
+
+export async function removeTasks(tasksToRemove: Task[]): Promise<void> {
+  console.log('🚀 ~ do usunięcia:')
+  tasksToRemove.forEach(task => {
+    console.log(task.title, task.date)
+  })
+  try {
+    const storedTasks = await AsyncStorage.getItem('tasks')
+    if (!storedTasks) return
+
+    const parsedTasks: Task[] = JSON.parse(storedTasks)
+
+    const updatedTasks = parsedTasks.filter(
+      task => !tasksToRemove.some(toRemove => toRemove.title === task.title)
+    )
+
+    await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks))
+  } catch (error) {
+    console.error('Error removing tasks:', error)
+  }
+}
+
+export async function toggleTaskCompletionAsync(title: string): Promise<void> {
+  try {
+    const stored = await AsyncStorage.getItem('tasks')
+    if (!stored) return
+
+    const tasks = JSON.parse(stored)
+    const updatedTasks = tasks.map((task: Task) =>
+      task.title === title
+        ? {
+            ...task,
+            completed: !task.completed,
+            dateOfCompletion: !task.completed
+              ? new Date().toLocaleDateString('en-CA')
+              : null,
+          }
+        : task
+    )
+
+    await AsyncStorage.setItem('tasks', JSON.stringify(updatedTasks))
+  } catch (e) {
+    console.error('Błąd podczas zapisywania zadania:', e)
+  }
+}
+
+export async function checkCyclicTasks(
+  day: Date,
+  cyclicTasks: string
+): Promise<void> {
+  const todayWeekDay = day
+    .toLocaleDateString('en-US', { weekday: 'short' })
+    .toLowerCase() // np. 'mon'
+  const todayDate = day.getDate() // dzień miesiąca
+
+  const parsedCyclicTasks = JSON.parse(cyclicTasks)
+  const cyclicTasksForThatDay: CyclicTask[] = []
+
+  parsedCyclicTasks.forEach((task: CyclicTask) => {
+    const baseDate = new Date(task.date)
+    const diffDays = Math.floor(
+      (day.getTime() - baseDate.getTime()) / (1000 * 60 * 60 * 24)
+    )
+
+    switch (task.type) {
+      case 0:
+        //@ts-ignore // co X dni
+        if (diffDays % task.period === 0)
+          //dodaj instancję do listy
+          cyclicTasksForThatDay.push(task)
+
+        break
+
+      case 1:
+        // w określone dni tygodnia
+        if (task?.weekDays?.includes(todayWeekDay as Day))
+          cyclicTasksForThatDay.push(task)
+
+        break
+
+      case 2:
+        // w określone dni miesiąca
+        if (task?.monthDays?.includes(todayDate))
+          cyclicTasksForThatDay.push(task)
+
+        break
+    }
+  })
+  //** jeśli wypadają, to są tworzone ich instancje z odpowiednią datą, o ile nie zostały już wcześniej utworzone */
+  await setCyclicInstance(cyclicTasksForThatDay, day)
+}
+
+export function getTasksToRemove(
+  oldTasks: Task[],
+  uniqueTasksState: Task[]
+): Task[] {
+  return oldTasks.filter(
+    (task: Task) =>
+      (task.dateOfCompletion &&
+        task.dateOfCompletion < new Date().toLocaleDateString('en-CA')) ||
+      (uniqueTasksState.some(t => t.title === task.title) &&
+        task.parent !== null)
+  )
+}
+
+export function getOldTasks(parsedTasks: Task[]): Task[] {
+  return parsedTasks.filter(
+    (task: Task) => task.date < new Date().toLocaleDateString('en-CA')
+  )
+}
+
+export function getOverdueTasks(oldTasks: Task[], day: Date): Task[] {
+  return oldTasks.filter(
+    (task: Task) =>
+      (!task.dateOfCompletion &&
+        day.toLocaleDateString('en-CA') ===
+          new Date().toLocaleDateString('en-CA')) ||
+      task.dateOfCompletion === day.toLocaleDateString('en-CA')
+  )
+}
+
+export function getTasksState(
+  overdueTasks: Task[],
+  parsedTasks: Task[],
+  day: Date
+): Task[] {
+  return [
+    ...overdueTasks,
+    ...parsedTasks.filter(
+      (task: Task) => task.date === day.toLocaleDateString('en-CA')
+    ),
+  ]
+}
